9436ad13a0b3d9f9d88f7cc4ae3c3d0a3fc46fce
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-10-16 10:51:11 -0300

    [Feature] Integração com Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/layout'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack develop based in Brazil!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexgow={1}>
+          <Heading as="h2" variant="page-title">
+            Luis Carlos
+          </Heading>
+          <p>Digital Craftzman ( Developer / Designer)</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
