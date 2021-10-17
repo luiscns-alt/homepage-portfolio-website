be54ed2e80e707802c60a55f2a397e693658719f
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-17 19:54:22 -0300

    [Feature] Adiconado seção Bio.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import NextLink from 'next/link'
 import {
+  Button,
   Container,
   Box,
   Heading,
   Image,
+  Link,
   useColorModeValue
 } from '@chakra-ui/react'
+import Paragraph from '../components/paragraph'
 import Section from '../components/section'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -49,7 +55,44 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          Luis Carlos
+          <NextLink href="/works/inkdrop">
+            <Link>Inkdrop</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1996</BioYear>
+          Born in Santarém - PA, Brazil.
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Complete Science Computation in UFOPA(Federal University of Western
+          Pará)
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Worked at Jornada Acadêmica
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ❤️
+        </Heading>
+        <Paragraph>Art, Music, Programming</Paragraph>
       </Section>
     </Container>
   )
