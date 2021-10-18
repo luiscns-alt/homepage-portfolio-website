482fb30ad135f890e071f37fa6215a539b4cf5fb
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-17 23:36:48 -0300

    [Feature] Implementado transições de página com Framer Motion.

--- pages/index.js
+++ pages/index.js
@@ -12,89 +12,92 @@ import Paragraph from '../components/paragraph'
 import Section from '../components/section'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack develop based in Brazil!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexgow={1}>
-          <Heading as="h2" variant="page-title">
-            Luis Carlos
-          </Heading>
-          <p>( Developer )</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/profile.jpg"
-            alt="Profile image"
-          />
+          Hello, I&apos;m a full-stack develop based in Brazil!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Luis Carlos
-          <NextLink href="/works/inkdrop">
-            <Link>Inkdrop</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My Portfolio
-            </Button>
-          </NextLink>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexgow={1}>
+            <Heading as="h2" variant="page-title">
+              Luis Carlos
+            </Heading>
+            <p>( Developer )</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/profile.jpg"
+              alt="Profile image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1996</BioYear>
-          Born in Santarém - PA, Brazil.
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Complete Science Computation in UFOPA(Federal University of Western
-          Pará)
-        </BioSection>
-        <BioSection>
-          <BioYear>2021</BioYear>
-          Worked at Jornada Acadêmica
-        </BioSection>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ❤️
-        </Heading>
-        <Paragraph>Art, Music, Programming</Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Luis Carlos
+            <NextLink href="/works/inkdrop">
+              <Link>Inkdrop</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1996</BioYear>
+            Born in Santarém - PA, Brazil.
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Complete Science Computation in UFOPA(Federal University of Western
+            Pará)
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Worked at Jornada Acadêmica
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ❤️
+          </Heading>
+          <Paragraph>Art, Music, Programming</Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
